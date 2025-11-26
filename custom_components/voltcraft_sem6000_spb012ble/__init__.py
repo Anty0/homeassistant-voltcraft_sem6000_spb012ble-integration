b3bc8bf64861ea4cfa3e09cfc8a70d550496007b
Jiří Kuchyňka (Anty) <anty150@gmail.com>
2025-11-26 11:07:07 +0100

    chore: fix lint

--- custom_components/voltcraft_sem6000_spb012ble/__init__.py
+++ custom_components/voltcraft_sem6000_spb012ble/__init__.py
@@ -4,9 +4,6 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
-
-
 PLATFORMS: list[Platform] = [Platform.SWITCH]
 
 
